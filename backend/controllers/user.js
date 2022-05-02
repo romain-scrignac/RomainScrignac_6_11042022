5a427ba7b6e2a56353db3e0c6bc252640a4cc214
Romain Scrignac <romain.scrignac@protonmail.com>
2022-05-02 15:11:17 +0200

    Validation backend du formulaire signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,17 +9,25 @@ const User = require('../models/User');
 
 // Middleware pour l'enregistrement des utilisateurs (hashage du mdp + envoi dans la bdd)
 exports.signup = (req, res) => {
-    bcrypt.hash(req.body.password, 10)      // On "sale" le mot de passe 10 fois par mesure de sécurité
+    const regexEmail = /^([a-z0-9]{1,20})([\.|_|-]{1}[a-z0-9]{1,20})?@{1}([a-z0-9]{2,15})\.[a-z]{2,4}$/;
+    // Vérification du formulaire, si c'est bon on crée le nouvel utilisateur
+    if (!req.body.email.match(regexEmail)) {
+        res.status(400).json({ error: 'Format d\'email invalide !' });
+    } else if (req.body.password.length < 5) {
+        res.status(400).json({ error: 'Mot de passe trop court !' });
+    } else {
+        bcrypt.hash(req.body.password, 10)      // On "sale" le mot de passe 10 fois par mesure de sécurité
         .then(hash => {
             const user = new User({         // On crée un nouvel utilisateur
                 email: req.body.email,
                 password: hash
             });
             user.save()                     // On l'enregistre dans la bdd
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))    // code 201 (nouvel objet créé)
-                .catch(error => res.status(400).json({ error }));                       // code 400 (erreur requête client)
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));   // code 500 (erreur serveur)
+        .catch(error => res.status(500).json({ error }));
+    }
 };
 
 // Middleware pour la connection des utilisateurs
